429306159c7fb45176ce2b743f52adea84a38bc5
Stefan Härtel <haertel.dev@gmail.com>
2023-12-03 14:28:14 +0100

    Test passed

--- 03.py
+++ 03.py
@@ -1,10 +1,13 @@
-input = open("03-input-test.txt", "r")
+input = open("03-input.txt", "r")
 lines = input.read().splitlines()
+
+LINES_NUM = len(lines)
+LINE_LENGTH = len(lines[0])
 
 # build the 2D data array
 data = []
 symbols = set()
-numbers = []
+numbersWithAdjacents = []
 
 class Coordinates:
   def __init__(self, x, y):
@@ -12,10 +15,43 @@ class Coordinates:
     self.y = y
 
 class NumberWithAdjacents:
-    def __init__(self, value, adjacents):
+    def __init__(self, value, adjacentCoordinates):
       self.value = value
-      self.adjacents = adjacents
+      self.adjacentCoordinates = adjacentCoordinates
 
+    def __repr__(self):
+      return f"{self.value}"
+
+    def isAdjacentToSymbol(self):
+      for coordinate in self.adjacentCoordinates:
+        if coordinate.x < 0 or coordinate.x >= LINE_LENGTH:
+          continue
+        if coordinate.y < 0 or coordinate.y >= LINES_NUM:
+          continue
+        if data[coordinate.y][coordinate.x] in symbols:
+          return True
+      return False
+
+def getAdjacents(x, y):
+  adjacents = set()
+
+  # same row
+  adjacents.add(Coordinates(x - 1, y + 0))
+  adjacents.add(Coordinates(x + 1, y + 0))
+
+  # top row
+  adjacents.add(Coordinates(x - 1, y + 1))
+  adjacents.add(Coordinates(x + 0, y + 1))
+  adjacents.add(Coordinates(x + 1, y + 1))
+
+  #bottom row
+  adjacents.add(Coordinates(x - 1, y - 1))
+  adjacents.add(Coordinates(x + 0, y - 1))
+  adjacents.add(Coordinates(x + 1, y - 1))
+
+  return adjacents
+
+# prepare data and extract symbols
 for line in lines:
   lineData = []
   for character in line:
@@ -24,16 +60,31 @@ for line in lines:
       symbols.add(character)
   data.append(lineData)
 
-print(*data, sep="\n")
-
+# extract numbers and adjacent coordinates
 for y, rowValue in enumerate(data):
   digits = ""
+  adjacents = set()
   for x, columnValue in enumerate(rowValue):
     if columnValue.isdigit():
       digits += columnValue
+      for adjacent in getAdjacents(x, y):
+        adjacents.add(adjacent)
     if not columnValue.isdigit() and digits != "":
-      numbers.append(int(digits))
+      numbersWithAdjacents.append(
+        NumberWithAdjacents(int(digits), adjacents)
+      )
       digits = ""
+      adjacents = set()
 
-print(symbols)
-print(numbers)
+sum = 0
+
+for number in numbersWithAdjacents:
+  if number.isAdjacentToSymbol():
+    print(number.value)
+    sum += number.value
+
+# print(symbols)
+# print(numbersWithAdjacents)
+# print(sum)
+
+# print(*data, sep="\n")
